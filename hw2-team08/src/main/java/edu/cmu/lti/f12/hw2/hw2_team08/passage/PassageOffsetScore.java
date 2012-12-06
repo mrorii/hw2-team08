6346344884b2b1ac89584f1c9b3763a62c29fcaf
Naoki Orii <mrorii@gmail.com>
2012-12-06 13:48:08 -0500

    Fix indentation
    Replace tabs with spaces

--- hw2-team08/src/main/java/edu/cmu/lti/f12/hw2/hw2_team08/passage/PassageOffsetScore.java
+++ hw2-team08/src/main/java/edu/cmu/lti/f12/hw2/hw2_team08/passage/PassageOffsetScore.java
@@ -1,10 +1,10 @@
 package edu.cmu.lti.f12.hw2.hw2_team08.passage;
 
 public class PassageOffsetScore implements KeytermWindowScorer {
-	@Override
-	public double scoreWindow(int begin, int end, int matchesFound,
-			int totalMatches, int keytermsFound, int totalKeyterms,
-			int textSize) {
-		return ( (double)textSize - (double)begin ) / (double)textSize;
-	}
+  @Override
+  public double scoreWindow(int begin, int end, int matchesFound,
+      int totalMatches, int keytermsFound, int totalKeyterms,
+      int textSize) {
+    return ( (double)textSize - (double)begin ) / (double)textSize;
+  }
 }
